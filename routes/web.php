d4076d38d09061023320701a80f5ccd301e20fe4
Isaac Caliz <isaaccaliz305@gmail.com>
2025-11-10 20:39:29 -0600

    no autorizado

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return response()->json([
+        'message' => 'Debe iniciar sesión para acceder a este recurso.'
+    ], 401);
+})->name('login');
